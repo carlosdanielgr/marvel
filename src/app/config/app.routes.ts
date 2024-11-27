d1fb7563670db46de90eee06e23f86f08844fa97
carlosdanielgr <carlos.dgr.12@gmail.com>
2024-11-26 20:15:29 -0500

    ui register completed

--- src/app/config/app.routes.ts
+++ src/app/config/app.routes.ts
@@ -5,4 +5,9 @@ export const routes: Routes = [
     path: '',
     loadComponent: () => import('./../presentation/auth/login/login.component'),
   },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./../presentation/auth/register/register.component'),
+  },
 ];
