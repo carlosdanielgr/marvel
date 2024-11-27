6db2ea521a710ca06f90da30ad75232ba4cdb486
carlosdanielgr <carlos.dgr.12@gmail.com>
2024-11-26 20:45:25 -0500

    footer component created

--- src/app/presentation/products/products.component.ts
+++ src/app/presentation/products/products.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss',
 })
